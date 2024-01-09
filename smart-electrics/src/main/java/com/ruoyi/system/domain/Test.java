bc9a024ff98c42b71b7f443703cf80c4ff685eab
31837 <22920202204684@stu.xmu.edu.cn>
2024-01-09 21:27:13 +0800

    新增了smart-electrics（智能配电箱）模块，并添加了一个test样例。 之后如果要添加新的页面，直接添加到smart-electrics的controller，mapper等等下面就行。

---
+++ smart-electrics/src/main/java/com/ruoyi/system/domain/Test.java
@@ -0,0 +1,65 @@
+package com.ruoyi.system.domain;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import com.ruoyi.common.annotation.Excel;
+import com.ruoyi.common.core.domain.BaseEntity;
+
+/**
+ * 测试对象 test
+ * 
+ * @author ruoyi
+ * @date 2024-01-09
+ */
+public class Test extends BaseEntity
+{
+    private static final long serialVersionUID = 1L;
+
+    /** $column.columnComment */
+    private Long id;
+
+    /** 姓名 */
+    @Excel(name = "姓名")
+    private String name;
+
+    /** 密码 */
+    @Excel(name = "密码")
+    private String password;
+
+    public void setId(Long id) 
+    {
+        this.id = id;
+    }
+
+    public Long getId() 
+    {
+        return id;
+    }
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+
+    public String getName() 
+    {
+        return name;
+    }
+    public void setPassword(String password) 
+    {
+        this.password = password;
+    }
+
+    public String getPassword() 
+    {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+            .append("id", getId())
+            .append("name", getName())
+            .append("password", getPassword())
+            .toString();
+    }
+}
